02997c31f07773d5649c9bd6dfeed03c3b8e27ff
shiuly-28 <shulybd1245@gmail.com>
2025-10-12 17:57:54 +0600

    filter page add

--- src/components/todoFilter.jsx
+++ src/components/todoFilter.jsx
@@ -1,30 +1,30 @@
-// import React from 'react'
-// import {CheckCircle, Clock, List} from 'lucide-react';
-//
-// export const TodoFilter = ({stats, filter}) => {
-//   const filters = [
-//     {key: 'all', label: 'All', icon: List, count: stats.total },
-//     {key: 'active', label: 'Active', icon: Clock, count: stats.active },
-//     {key: 'completed', label: 'Completed', icon: CheckCircle, count: stats.completed  },
-//   ]
-//   return (
-//     <div className='flex items-center justify-center'>
-//       <div className='inline-flex bg-gray-200 rounded-lg p-1'>
-//         {
-//           filters.map(({key, label, icon: Icon, count }) => (
-//             <button key={key} className={`flex items-center gap-2 py-2 px-2 rounded-md text-sm font-medium
-//             transition-all duration-200 ${filter === key ? 'bg-white text-gray-800 shadow-md' :
-//                 'text-gray-700 hover:text-gray-800 hover:bg-gray-300'
-//                 }`}>
-//               <Icon size={16}/>
-//               <span>{label}</span>
-//                 <span>{count}</span>
-//             </button>
-//           ))
-//
-//
-//         }
-//       </div>
-//     </div>
-//   )
-// }
+import React from 'react'
+import {CheckCircle, Clock, List} from 'lucide-react';
+
+export const TodoFilter = ({stats, filter}) => {
+  const filters = [
+    {key: 'all', label: 'All', icon: List, count: stats.total },
+    {key: 'active', label: 'Active', icon: Clock, count: stats.active },
+    {key: 'completed', label: 'Completed', icon: CheckCircle, count: stats.completed  },
+  ]
+  return (
+    <div className='flex items-center justify-center'>
+      <div className='inline-flex bg-gray-200 rounded-lg p-1'>
+        {
+          filters.map(({key, label, icon: Icon, count }) => (
+            <button key={key} className={`flex items-center gap-2 py-2 px-2 rounded-md text-sm font-medium
+            transition-all duration-200 ${filter === key ? 'bg-white text-gray-800 shadow-md' :
+                'text-gray-700 hover:text-gray-800 hover:bg-gray-300'
+                }`}>
+              <Icon size={16}/>
+              <span>{label}</span>
+                <span>{count}</span>
+            </button>
+          ))
+
+
+        }
+      </div>
+    </div>
+  )
+}
