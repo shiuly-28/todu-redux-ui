baed9d2470a99c32ea8a39377721b0719c584a0f
shiuly-28 <shulybd1245@gmail.com>
2025-10-12 17:41:28 +0600

    first commit

--- src/store/todoSlice.js
+++ src/store/todoSlice.js
@@ -1,31 +1,34 @@
-import {createSlice} from '@reduxjs/toolkit';
-
-const initialState = {
-  items: [],
-  filter: 'all',
-  isAddingTodo: false
-}
-
-const todoSlice = createSlice({
-  name: 'todos',
-  initialState,
-  reducers: {
-    setIsAddingTodo: (state, action) => {
-      state.isAddingTodo = action.payload
-    },
-    addTodo: (state, action) => {
-      const newTodo = {
-        id: crypto.randomUUID(),
-        text: action.payload,
-        completed: false,
-        createdAt: new Date().toISOString(),
-        updatedAt: new Date().toISOString()
-      }
-      state.items.unshift(newTodo)
-      state.isAddingTodo = false
-
-    }
-  }
-})
-export const {setIsAddingTodo, addTodo} = todoSlice.actions
-export default todoSlice.reducer
+// import {createSlice} from '@reduxjs/toolkit';
+//
+// const initialState = {
+//   items: [],
+//   filter: 'all',
+//   isAddingTodo: false
+// }
+//
+// const todoSlice = createSlice({
+//   name: 'todos',
+//   initialState,
+//   reducers: {
+//     setIsAddingTodo: (state, action) => {
+//       state.isAddingTodo = action.payload
+//     },
+//     addTodo: (state, action) => {
+//       const newTodo = {
+//         id: crypto.randomUUID(),
+//         text: action.payload,
+//         completed: false,
+//         createdAt: new Date().toISOString(),
+//         updatedAt: new Date().toISOString()
+//       }
+//       state.items.unshift(newTodo)
+//       state.isAddingTodo = false
+//
+//     },
+//       setFiler: (state, action) => {
+//         state.isAddingTodo = action.payload
+//       }
+//   }
+// })
+// export const {setIsAddingTodo, addTodo, setFiler} = todoSlice.actions
+// export default todoSlice.reducer
